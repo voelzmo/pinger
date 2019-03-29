6151013d2b15b061f698e3660f48d53b8e9ab85f
Marco Voelz <marco.voelz@sap.com>
2019-03-29 10:52:25 +0100

    Remove comments

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/facebookgo/pidfile"
-	"github.com/spf13/viper"
 	"log"
 	"time"
+
+	"github.com/facebookgo/pidfile"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -33,21 +34,7 @@ func (c *HTTPConfig) isSecure() bool {
 }
 
 func init() {
-	//flag.IntVar(&interval, "interval", 10, "The interval between two pings in seconds")
-	//flag.StringArrayVar(&addressesToPing, "address", []string{}, "The address which should be pinged. Format (http|https)://<IP>:<port>")
-	//flag.Float64Var(&errorRate, "error-rate", 0.0, "error rate in percent")
-	//flag.StringVar(&pidFilePath, "pidfile-path", "", "Path to write a PID file to")
 	flag.StringVar(&configPath, "config-path", "", "Absolute path to read a config file from")
-	//flag.StringVar(&serverConfig.certFile, "cert-path", "", "Path to certificate for https server")
-	//flag.StringVar(&serverConfig.keyFile, "key-path", "", "Path to key for https server")
-	//flag.StringVar(&serverConfig.caCertFile, "ca-cert-path", "", "Path to custom ca to trust")
-
-	//portFromEnv := os.Getenv("PORT")
-	//defaultPort := 8080
-	//if portFromEnv != "" {
-	//	defaultPort, _ = strconv.Atoi(portFromEnv)
-	//}
-	//flag.IntVar(&serverConfig.port, "listenPort", defaultPort, "The port to listen on")
 }
 
 func main() {
@@ -81,7 +68,6 @@ func readConfiguration() {
 			panic(fmt.Errorf("fatal error config file at '%s': %s", configPath, err))
 		}
 	}
-	//viper.BindPFlags(flag.CommandLine)
 }
 
 func writePIDFile(path string) {
