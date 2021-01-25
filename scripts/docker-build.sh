89bf2b65ea5462fde3bafdc99b04062e2c37d516
Marco Voelz <voelzmo@users.noreply.github.com>
2021-01-25 16:15:59 +0100

    Update docker-build.sh

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -11,18 +11,10 @@ set -e
 # set debug mode
 #set -x
 
-
 # build the new docker image
 #
 echo '>>> Building new image'
-# Due to a bug in Docker we need to analyse the log to find out if build passed (see https://github.com/dotcloud/docker/issues/1875)
-docker build --no-cache=true -t $REPOSITORY/$PROJECT:${VERSION} . | tee /tmp/docker_build_result.log
-RESULT=$(cat /tmp/docker_build_result.log | tail -n 1)
-if [[ "$RESULT" != *Successfully* ]];
-then
-  exit -1
-fi
-
+docker build --no-cache=true -t $REPOSITORY/$PROJECT:${VERSION} . 
 
 echo '>>> Push new image'
 docker push $REPOSITORY/$PROJECT:${VERSION}
